3094545076e5aa0f6dd2f67f346dd5f889d4787c
Yuvraj Singh <yuvi1707@hotmail.com>
2024-06-25 21:57:13 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,13 +14,20 @@ app.use(express.json());
 //Option 1: ALLOW all origins with default of cors(*)
 // app.use(cors());
 // Option 2: Allow custom origins
-app.use(
-    cors({
-        origin: 'https://bookstore-mern-eight.vercel.app',
-        methods: ['GET','POST','PUT','DELETE'],
-        credentials: true
-    })
-);
+// app.use(
+//     cors({
+//         origin: 'https://bookstore-mern-eight.vercel.app',
+//         methods: ['GET','POST','PUT','DELETE'],
+//         credentials: true
+//     })
+// );
+// Add CORS headers
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://bookstore-mern-eight.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.get("/",(request,response)=>{
     console.log(request);
